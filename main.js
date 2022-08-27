908d72888abf3bd7a1c04b9f5f5d024e7279d526
dankarger <karger.dan@gmail.com>
2022-08-27 19:32:22 +0300

    add env

---
+++ main.js
@@ -0,0 +1,75 @@
+const searchForm = document.querySelector("#search-form");
+const searchFormatInput = searchFormInput = searchForm.querySelector("input");
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+
+if (SpeechRecognition) {
+    console.log("browser support");
+
+    searchForm.insertAdjacentHTML("beforeend",'          <button type="button" class="icon mic-off">MIC ICON</button>\n')
+    const micBtn = searchForm.querySelector("button");
+    const micIcon = searchForm.querySelector(".icon");
+
+    const recognition = new SpeechRecognition();
+    recognition.continuous = true ;
+    // recognition.lang = "he"
+
+    console.log('lng',SpeechRecognition.lang)
+    micBtn.addEventListener('click',micBtnClick);
+    function micBtnClick() {
+        if(micIcon.classList.contains('mic-off')){
+            recognition.start();
+        }
+        else {
+
+            recognition.stop();
+        }
+    }
+    recognition.addEventListener("start", startSpeechRecognition);// <=> recognition.onstart = function(){...}
+    function startSpeechRecognition(){
+        micIcon.classList.remove('mic-off');
+        micIcon.classList.add('mic-on');
+        searchFormatInput.focus();
+        console.log("Speech Recognition Active")
+    }
+    recognition.addEventListener("end", endSpeechRecognition);// <=> recognition.onend = function(){...}
+    function endSpeechRecognition(){
+        micIcon.classList.remove('mic-on');
+        micIcon.classList.add('mic-off');
+        searchFormatInput.focus();
+        console.log("Speech Recognition Disconnected")
+    }
+
+    recognition.addEventListener("result",resultOfSpeechRecognition);// <=> recognition.onresult = function(){...}
+    function resultOfSpeechRecognition(event){
+        console.log(event);
+        const currentResult = event.resultIndex
+        const transcript = event.results[currentResult][0].transcript
+        // searchFormatInput.value = transcript;
+
+        if(transcript.toLowerCase().trim()==='stop recording') {
+            recognition.stop()
+        }
+        else if(!searchFormatInput.value) {
+            searchFormatInput.value = transcript
+        }
+        else {
+            if(transcript.toLowerCase().trim()==='go') {
+                searchForm.action = `https://unsplash.com/s/photos/${transcript}`
+                searchForm.submit()
+            }
+            else if(transcript.toLowerCase().trim()==='reset input') {
+                searchFormatInput.value = '';
+            }
+            else {
+                searchFormatInput.value = transcript
+            }
+        }
+        // setTimeout(()=>{
+        //     searchForm.submit();
+        // },750)
+    }
+}
+else {
+    console.log("browser dont support")
+}
