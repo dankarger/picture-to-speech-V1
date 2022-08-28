556a70036f7e334a5541f829d5b0d00538b341f4
dankarger <karger.dan@gmail.com>
2022-08-28 18:51:59 +0300

    add responsive font size

--- clientVanilla/textGenerator.js
+++ clientVanilla/textGenerator.js
@@ -1,6 +1,11 @@
 const textGenerator =(text)=>{
     const textDiv = document.querySelector('#text-div');
     const textElement = textDiv.querySelector('.text');
+    const fontSize = (textDiv.clientWidth / text.length);
+    // if(fontSize<100)
+        textElement.style.fontSize = `${fontSize}px`;
+
+    console.log('font',fontSize, textDiv.clientWidth)
     textElement.textContent = text;
     // textDiv.appendChild(textDiv)
 }
