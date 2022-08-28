94c19739028af4c15cad55d54aead0e36dbef7bd
dankarger <karger.dan@gmail.com>
2022-08-28 20:18:52 +0300

    add stage 4 - position 2

--- clientVanilla/textGenerator.js
+++ clientVanilla/textGenerator.js
@@ -17,16 +17,16 @@ const colorText = (color) => {
 }
 
 const textPosition = (position) => {
-    if (position === ' up') {
+    if (position === ' top' || position === 'up') {
         textDiv.classList.remove('position---center');
         textDiv.classList.remove('position---bottom');
         textDiv.classList.add('position--top');
-    }else if (position=== ' center') {
+    }else if (position=== ' center' || position === 'middle') {
         textDiv.classList.remove('position---top');
         textDiv.classList.remove('position---bottom');
         textDiv.classList.add('position--center');
     }
-    else if (position=== ' down') {
+    else if (position=== ' bottom' || position === 'down') {
         textDiv.classList.remove('position---top');
         textDiv.classList.remove('position---center');
         textDiv.classList.add('position--bottom');
